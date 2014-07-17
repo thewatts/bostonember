019cca3a2fb617259380484429a381e336f9fbac
Nathaniel Watts <reg@nathanielwatts.com>
2014-07-17 10:09:27 -0500

    I BROKE IT

---
+++ ember/tests/integration/speakers-page-test.js
@@ -0,0 +1,29 @@
+import startApp from 'bostonember/tests/helpers/start-app';
+
+var App, server;
+
+module('Integration - Speaker Page', {
+  // setup: function() {
+  //   App = startApp();
+  //   var speakers = [
+  //     {
+  //       id: 1,
+  //       name: 'Bugs Bunny'
+  //     },
+  //     {
+  //       id: 2,
+  //       name: 'Wile E. Coyote'
+  //     },
+  //     {
+  //       id: 3,
+  //       name: 'Yosemite Same'
+  //     }
+  //   ];
+
+  //   server = new Pretender(function() {
+  //     this.get('/api/speakers', function(request) {
+  //       return [200, {"Content-Type": "application/json"}, JSON.stringify({speakers: speakers})];
+  //     });
+  //   });
+  // }
+});
